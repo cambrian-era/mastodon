9e02dd2d707254e8339967d97dc8f4b91ff7a88f
Merisa Furchtgott <merisa.furchtgott@gmail.com>
2018-09-14 20:21:47 -0700

    Resolve conflict.

--- app/services/gif_embed_service.rb
+++ app/services/gif_embed_service.rb
@@ -11,11 +11,7 @@ class GifEmbedService < BaseService
       .get("http://api.giphy.com/v1/gifs/#{id}", :params => {
         :api_key => ENV['GIPHY_API_KEY']
       }) 
-<<<<<<< HEAD
     gif_url = JSON.parse(info_http.body().to_s)['data']['images']['original']['url']
-=======
-    gif_url = JSON.parse(info_http.body().to_s)['data']['images']['url']
->>>>>>> 94da6f5e1afc5606002f516761216003ab82aa31
 
     media_http = HTTP
       .accept('image/*')
