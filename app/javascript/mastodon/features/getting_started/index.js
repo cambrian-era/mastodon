e5082026ca96db9026d958d4e2502286d459881c
LAPTOP-8G9F4126\meris <merisa.furchtgott@gmail.com>
2018-11-15 08:44:44 -0800

    Fixed a minor issue with markdown, changed github link to point to this repo, locked Paperclip at 6.0 to resolve issues.

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -152,7 +152,7 @@ class GettingStarted extends ImmutablePureComponent {
               <FormattedMessage
                 id='getting_started.open_source_notice'
                 defaultMessage='Mastodon is open source software. You can contribute or report issues on GitHub at {github}.'
-                values={{ github: <span><a href='https://github.com/tootsuite/mastodon' rel='noopener' target='_blank'>tootsuite/mastodon</a> (v{version})</span> }}
+                values={{ github: <span><a href='https://github.com/cambrian-era/mastodon' rel='noopener' target='_blank'>tootsuite/mastodon</a> (v{version})</span> }}
               />
             </p>
           </div>
